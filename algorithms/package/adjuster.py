30fdaacbb28de94aa2df2edd183bf8c4789b1265
khhha-pffuuuu <serhiogames@mail.ru>
2023-11-08 10:17:30 +0300

    Sort folder renaming

--- algorithms/package/adjuster.py
+++ algorithms/package/adjuster.py
@@ -1,6 +1,6 @@
-from .sort.insertion_sort import insertion_sort
-from .sort.merge_sort import merge_sort
-from .sort.quick_sort import quick_sort
-from .sort.radix_sort import radix_sort
+from .sort_algs.insertion_sort import insertion_sort
+from .sort_algs.merge_sort import merge_sort
+from .sort_algs.quick_sort import quick_sort
+from .sort_algs.radix_sort import radix_sort
 
 # Сборщик алгоритмов #
