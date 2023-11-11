50c4c6dd318fcf15757b258b58ee2763b8e71f7c
khhha-pffuuuu <serhiogames@mail.ru>
2023-11-11 07:13:58 +0300

    Хэш для строк

--- algorithms/package/adjuster.py
+++ algorithms/package/adjuster.py
@@ -5,6 +5,8 @@ from .sort_algs.quick_sort import quick_sort
 from .sort_algs.radix_sort import radix_sort
 
 # Алгоритмы для работы со строками
-from .string_algs.equality_substrings import is_equal
+from .string_algs.equality_substrings import are_equals
+from .string_algs.string_base import str_base
+from .string_algs.z_function import search_substrings
 
 # --Сборщик алгоритмов-- #
